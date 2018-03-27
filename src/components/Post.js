2c6dcb4f253a9bbb8d0da06ef7e923b6ff5f8ffa
Thiago Resende <thiago.gcresende@gmail.com>
2018-03-27 20:40:01 -0300

    Botão de like reagindo ao toque

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,7 @@ import {
     Image,
     View,
     Dimensions,
+    TouchableOpacity,
     StyleSheet
 } from 'react-native';
 
@@ -12,15 +13,42 @@ let size = { width: width, height: width }
 
 export default class Post extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            foto: this.props.foto
+        }
+    }
+
+    carregaIcone(likeada) {
+        return likeada ? require('../../resources/img/s2-checked.png') : require('../../resources/img/s2.png')
+    }
+
+    like() {
+        const fotoAtualizada = {
+            ...this.state.foto,
+            likeada: !this.state.foto.likeada
+        }
+        this.setState({ foto: fotoAtualizada })
+    }
+
     render() {
+
+        const foto = this.state.foto;
         return (
             <View>
                 <View style={styles.cabecalho}>
-                    <Image source={{ uri: this.props.foto.urlPerfil }}
+                    <Image source={{ uri: foto.urlPerfil }}
                         style={styles.fotoDePerfil} />
-                    <Text>{this.props.foto.loginUsuario}</Text>
+                    <Text>{foto.loginUsuario}</Text>
                 </View>
-                <Image source={{ uri: this.props.foto.urlFoto }} style={styles.foto} />
+                <Image source={{ uri: foto.urlFoto }} style={styles.foto} />
+                <View style={styles.rodape}>
+                    <TouchableOpacity onPress={this.like.bind(this)}>
+                        <Image style={styles.botaoDeLike}
+                            source={this.carregaIcone(foto.likeada)} />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -39,5 +67,12 @@ const styles = StyleSheet.create({
     foto: {
         width: width,
         height: width
+    },
+    botaoDeLike: {
+        width: 40,
+        height: 40
+    },
+    rodape: {
+        margin: 10
     }
 })
