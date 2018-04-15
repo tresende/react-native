4762f668e91abb4b6858ef0738bdc05c12722c40
Thiago Resende <thiago.gcresende@gmail.com>
2018-04-15 10:21:40 -0300

    Recebendo o Token

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -13,8 +13,28 @@ import {
 
 export default class Login extends Component {
 
-    login() {
-
+    efeturaLogin() {
+        const uri = "http://localhost:8080/api/public/login"
+        const requestInfo = {
+            method: 'POST',
+            headers: new Headers({
+                'Content-type': 'application/json'
+            }),
+            body: JSON.stringify({
+                login: this.state.usuario,
+                senha: this.state.senha
+            })
+        }
+        fetch(uri, requestInfo).then((response) => {
+            if (response.ok) {
+                return response.text();
+            }
+            else {
+                throw new Error("Não foi possível efetuar login");
+            }
+        }).then(token => {
+            console.warn(token);
+        })
     }
 
     render() {
@@ -23,14 +43,17 @@ export default class Login extends Component {
                 <Text style={styles.titulo}>Instalura</Text>
                 <View style={styles.form}>
                     <TextInput placeholder="Usuário ..."
+                        autoCapitalize="none"
                         onChangeText={texto => { this.setState({ usuario: texto }) }}
                         style={styles.input} />
 
                     <TextInput placeholder="Senha ..."
+                        autoCapitalize="none"
+                        secureTextEntry={true}
                         onChangeText={texto => { this.setState({ senha: texto }) }}
                         style={styles.input} />
 
-                    <Button title="Login" onPress={() => { this.login() }} />
+                    <Button title="Login" onPress={this.efeturaLogin.bind(this)} />
                 </View>
             </View>
         );
